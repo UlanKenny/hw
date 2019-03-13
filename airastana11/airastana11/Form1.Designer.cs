f7be652d3d61f1bfd25308282d6e474066c8838e
UlanKenny <35252999+UlanKenny@users.noreply.github.com>
2019-03-13 23:13:34 +0600

    Add files via upload

---
+++ airastana11/airastana11/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace airastana11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.retrieveBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.description = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.birth = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(485, 329);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(127, 25);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 1;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // retrieveBtn
+            // 
+            this.retrieveBtn.Location = new System.Drawing.Point(231, 25);
+            this.retrieveBtn.Name = "retrieveBtn";
+            this.retrieveBtn.Size = new System.Drawing.Size(75, 23);
+            this.retrieveBtn.TabIndex = 2;
+            this.retrieveBtn.Text = "Retrieve";
+            this.retrieveBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(350, 25);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 3;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(484, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(591, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(591, 127);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(100, 20);
+            this.nameTxt.TabIndex = 6;
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(591, 244);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(100, 20);
+            this.description.TabIndex = 8;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(591, 311);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(100, 20);
+            this.email.TabIndex = 9;
+            this.email.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // birth
+            // 
+            this.birth.Location = new System.Drawing.Point(550, 184);
+            this.birth.Name = "birth";
+            this.birth.Size = new System.Drawing.Size(200, 20);
+            this.birth.TabIndex = 10;
+            this.birth.Value = new System.DateTime(2019, 3, 13, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(588, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Birthday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(591, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(588, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Description";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.birth);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.retrieveBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button retrieveBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.DateTimePicker birth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ airastana11/airastana11/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace airastana11
+{
+
+    public partial class Form1 : Form
+    {
+
+        static string conString = "Server=localhost;Database=airastana;Uid=root;Pwd=;";
+        MySqlConnection con = new MySqlConnection(conString);
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //DATAGRIDVIEW PROPERTIES
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "ID";
+            dataGridView1.Columns[1].Name = "Name";
+            dataGridView1.Columns[2].Name = "Birth";
+            dataGridView1.Columns[3].Name = "Description";
+            dataGridView1.Columns[3].Name = "Email";
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            //SELECTION MODE
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+
+        }
+
+        //INSERT INTO DB
+        private void add(string name, string birth, string description, string email)
+        {
+            //SQL STMT
+            string sql = "INSERT INTO contacts(Name,Birth,Description,Email) VALUES(@NAME,@BIRTH,@DESCRIPTION,@EMAIL)";
+            cmd = new MySqlCommand(sql, con);
+
+            //ADD PARAMETERS
+            cmd.Parameters.AddWithValue("@NAME", name);
+            cmd.Parameters.AddWithValue("@BIRTH", birth);
+            cmd.Parameters.AddWithValue("@DESCRIPTION", description);
+            cmd.Parameters.AddWithValue("@EMAIL", email);
+            //OPEN CON AND EXEC insert
+            try
+            {
+                con.Open();
+
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    clearTxts();
+                    MessageBox.Show("Successfully Inserted");
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //ADD TO DGVIEW
+        private void populate(string id,string name, string birth, string description, string email)
+        {
+            dataGridView1.Rows.Add(id, name, birth, description,email);
+        }
+
+        //RETRIEVE FROM DB
+        private void retrieve()
+        {
+            dataGridView1.Rows.Clear();
+
+            //SQL STMT
+            string sql = "SELECT * FROM contacts ";
+            cmd = new MySqlCommand(sql, con);
+
+            //OPEN CON,RETRIEVE,FILL DGVIEW
+            try
+            {
+                con.Open();
+
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.Fill(dt);
+
+                //LOOP THRU DT
+                foreach (DataRow row in dt.Rows)
+                {
+                    populate(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[0].ToString(),row[0].ToString());
+                }
+
+                con.Close();
+
+                //CLEAR DT
+                dt.Rows.Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //UPDATE DB
+        private void update(Int32 id,string name, string birth, string description, string email)
+        {
+            //SQL STMT
+            string sql = "UPDATE peopleTB SET Name='" + name + "',Birth='" + birth + "',Description='" + description + "' WHERE ID=" + id + "";
+            cmd = new MySqlCommand(sql, con);
+
+            //OPEN CON,UPDATE,RETRIEVE DGVIEW
+            try
+            {
+                con.Open();
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.UpdateCommand = con.CreateCommand();
+                adapter.UpdateCommand.CommandText = sql;
+
+                if (adapter.UpdateCommand.ExecuteNonQuery() > 0)
+                {
+                    clearTxts();
+                    MessageBox.Show("Successfully Updated");
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+
+        }
+
+        //DELETE FROM DB
+        private void delete(int id)
+        {
+            //SQLSTMT
+            string sql = "DELETE FROM contacts WHERE ID=" + id + "";
+            cmd = new MySqlCommand(sql, con);
+
+            //'OPEN CON,EXECUTE DELETE,CLOSE CON
+            try
+            {
+                con.Open();
+                MessageBox.Show(con.State.ToString());
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.DeleteCommand = con.CreateCommand();
+
+                adapter.DeleteCommand.CommandText = sql;
+
+                //PROMPT FOR CONFIRMATION
+                if (MessageBox.Show("Sure ??", "DELETE", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        clearTxts();
+                        MessageBox.Show("Successfully deleted");
+                    }
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+
+        }
+
+        //clear txtx
+        private void clearTxts()
+        {
+            nameTxt.Text = "";
+            description.Text = "";
+            email.Text = "";
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            nameTxt.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            birth.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            description.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+
+            email.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            add(nameTxt.Text, birth.CustomFormat="dd-mm-yyyy", description.Text,email.Text);
+        }
+
+        private void retrieveBtn_Click(object sender, EventArgs e)
+        {
+            retrieve();
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            String selected = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            int id = Convert.ToInt32(selected);
+
+            update(id, nameTxt.Text, birth.CustomFormat="dd-mm-yyyy",description.Text, email.Text);
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            String selected = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            int id = Convert.ToInt32(selected);
+
+            delete(id);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
