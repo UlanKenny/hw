e1052ec0aac2a1c6a6614e066dcf4b324edf22d1
UlanKenny <35252999+UlanKenny@users.noreply.github.com>
2019-03-13 22:58:23 +0600

    Add files via upload

---
+++ pars/Core/Kurs/KursParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom.Html;
+
+namespace Parser.Core.Kurs
+{
+    class KursParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list  = new List<string>();
+            const string Selectors = "a";
+            var items = document.QuerySelectorAll(Selectors);
+            
+            foreach(var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ pars/Core/Kurs/KursSettings.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Parser.Core.Kurs
+{
+    class KursSettings : IParserSettings
+    {
+        public KursSettings(int start, int end)
+        {
+            StartPoint = start;
+            EndPoint = end;
+        }
+
+        public string BaseUrl { get; set; } = "https://kurs.kz";
+
+        public string Prefix { get; set; } = "{CurrentId}";
+
+        public int StartPoint { get; set; }
+
+        public int EndPoint { get; set; }
+    }
+}
